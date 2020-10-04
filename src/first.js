fb53225006917bb3c2b2c62f0c6ec22a81cbcfd9
Saurabh Banawar <saurabba@amdocs.com>
2020-10-04 17:51:51 +0530

    First commit

---
+++ src/first.js
@@ -0,0 +1,3 @@
+import {secondMsg} from './second.js';
+
+export const firstMsg = "I am first" + " " + secondMsg;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import {firstMsg} from './first.js';
+import './main.scss'
+
+console.log('Msg received from first.js is: ' + firstMsg);
+console.log('Script Loaded.')
+console.log('Hi')
